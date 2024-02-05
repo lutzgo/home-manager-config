232ab474803df7cdde1a53e4939a489a0ee07234
lutzgo <lutz0go@gmail.com>
2024-02-05 10:46:55 +0100

    removed old users/hosts and added sgo

--- home/toi/hostname/flores/sgo.nix
+++ home/toi/hostname/flores/sgo.nix
@@ -17,8 +17,8 @@
       feature = {
         gui = {
           enable = true;
-          displayServer = "x";
-          windowManager = "cinnamon";
+          displayServer = "wayland";
+          windowManager = "hyprland";
         };
         #theming.enable = false;
       };
